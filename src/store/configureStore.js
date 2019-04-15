d0754acf5a19a0c62da6bf09cbc0c11b76fd5813
gnun <gnunua@gmail.com>
2019-04-15 18:04:57 +0400

    add debugging

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,17 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, compose } from "redux";
 import placesReducer from "./reducers/places";
 
 const rootReducer = combineReducers({
   places: placesReducer
 });
 
+let composeEnhancer = compose;
+if (__DEV__) {
+  composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancer());
 };
 
 export default configureStore;
